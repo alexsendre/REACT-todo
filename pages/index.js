4fd482a62b68a26908ab39e97394412f6f5cbc28
Alex Sendre <rares.sendre@gmail.com>
2023-10-12 17:54:46 -0500

    functional authentication

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,13 @@ function Home() {
     >
       <h1>Hello {user.displayName}! </h1>
       <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
+      <Button
+        variant="danger"
+        type="button"
+        size="lg"
+        className="copy-btn"
+        onClick={signOut}
+      >
         Sign Out
       </Button>
     </div>
